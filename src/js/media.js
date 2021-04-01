3e215e2fef2fd12fc3d45c2e1239fde396ac47eb
Jef Schoolmeester <jef.schoolmeester99@gmail.com>
2021-04-01 10:22:56 +0200

    [FEAT] Medias

---
+++ src/js/media.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("canvas");
+const unCarre = canvas.getContext("2d");
+unCarre.fillStyle = "#C3D3F4";
+unCarre.fillRect(50, 0, 100, 110);
+unCarre.fillStyle = "#00AD5D";
+unCarre.fillRect(0, 50, 100, 50);
+unCarre.fillStyle = "#C71717";
+unCarre.fillRect(10, 10, 60, 30);
